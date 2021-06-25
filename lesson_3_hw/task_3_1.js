63facd907298f83ee832b630bdd02bbed6dad983
johnk2280 <johnk2280@gmail.com>
2021-06-25 22:38:33 +0500

    [add] added task_3_1

--- lesson_3_hw/task_3_1.js
+++ lesson_3_hw/task_3_1.js
@@ -2,3 +2,29 @@
  * 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
  */
 
+function getArray(num) {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
+function sieve(num) {
+    const arr = getArray(num);
+    let i = 3;
+    while (i <= num) {
+        if (arr[i] % 2 === 0) {
+            arr[i] = 0
+        } else {
+            for (let j = i + 1; j < num; j++) {
+                if (arr[j] % arr[i] === 0) {
+                    arr[j] = 0
+                }
+            }
+        }
+        i++
+    } return arr
+}
+
+console.log(sieve(100).slice(2,))
