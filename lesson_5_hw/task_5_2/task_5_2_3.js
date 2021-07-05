8a015a28d7e6927a628bd5abdc165554a3a133f2
johnk2280 <johnk2280@gmail.com>
2021-07-05 21:16:45 +0500

    [upd] code refactoring

--- lesson_5_hw/task_5_2/task_5_2_3.js
+++ lesson_5_hw/task_5_2/task_5_2_3.js
@@ -1,5 +1,19 @@
 'use strict'
 
+
+/**
+ * 2. Сделать генерацию корзины динамической: верстка корзины не должна находиться в HTML-структуре.
+ * Там должен быть только div, в который будет вставляться корзина, сгенерированная на базе JS:
+ * 2.1. Пустая корзина должна выводить строку «Корзина пуста»;
+ * 2.2. Наполненная должна выводить «В корзине: n товаров на сумму m рублей».
+ *
+ * 3*. Сделать так, чтобы товары в каталоге выводились при помощи JS:
+ * 3.1. Создать массив товаров (сущность Product);
+ * 3.2. При загрузке страницы на базе данного массива генерировать вывод из него.
+ * HTML-код должен содержать только div id=”catalog” без вложенного кода. Весь вид каталога генерируется JS.
+ * @type {{basketContainer: null, renderRow(*): HTMLDivElement, totalBasketPrice(): *, basketAmount: null, renderBasket(): void, renderTotalPriceRow(): HTMLDivElement, products: [{quantity: number, price: number, title: string}, {quantity: number, price: number, title: string}, {quantity: number, price: number, title: string}]}}
+ */
+
 const basket = {
     products: [
         {
