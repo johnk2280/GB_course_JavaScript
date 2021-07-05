b67b4432aadeb0edb59e119a84bc0bfbe30cddff
johnk2280 <johnk2280@gmail.com>
2021-07-05 21:09:43 +0500

    [add] added task_5_2

---
+++ lesson_5_hw/task_5_2/task_5_2.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const basket = {
+    products: [
+        {
+            'title': 'apple',
+            'price': 17.5,
+            'quantity': 5
+        },
+        {
+            'title': 'potato',
+            'price': 3.05,
+            'quantity': 7
+        },
+        {
+            'title': 'cucumber',
+            'price': 5.75,
+            'quantity': 5
+        }
+    ],
+
+    // products: [],
+
+    basketAmount: null,
+    basketContainer: null,
+
+    totalBasketPrice() {
+        for (let i = 0; i < this.products.length; i++) {
+            this.basketAmount += (this.products[i].price * this.products[i].quantity)
+        }
+        return +this.basketAmount.toFixed(2)
+    },
+
+    renderBasket() {
+        this.basketContainer = document.getElementById('basket');
+
+        for (let i = 0; i < this.products.length; i++) {
+            const productRow = this.renderRow(this.products[i]);
+            this.basketContainer.appendChild(productRow)
+        }
+
+        const totalPriceRow = this.renderTotalPriceRow()
+        this.basketContainer.appendChild(totalPriceRow)
+
+    },
+
+    renderRow(product) {
+        const div = document.createElement('div');
+        div.className = 'product_row'
+        div.innerHTML = `Наименование: ${product.title}; 
+                        Стоимость за ед.: ${product.price}; 
+                        Количество: ${product.quantity};
+                        Полная стоимость товара: ${product.price * product.quantity}`;
+        return div
+    },
+
+    renderTotalPriceRow() {
+        const div = document.createElement('div');
+        div.className = 'total_row'
+        if (this.products.length !== 0) {
+            div.innerHTML = `В корзине товаров: ${this.products.length}, на сумму: ${this.totalBasketPrice()}`;
+        } else {
+            div.innerHTML = 'Корзина пуста';
+        }
+        return div
+    },
+
+};
+
+basket.renderBasket()
