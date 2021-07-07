ef26f365016d6614bb2c69dcd64abfe70c1dd734
johnk2280 <johnk2280@gmail.com>
2021-07-07 23:14:28 +0500

    [add] added basket

--- lesson_6_hw/task_6_1.js
+++ lesson_6_hw/task_6_1.js
@@ -27,16 +27,17 @@ const catalog = {
     catalogContainer: null,
 
 
-    renderCatalog() {
+    init() {
         this.catalogContainer = document.getElementById('catalog');
+        this.catalogContainer.innerHTML = '<h2 class="div_title">Catalog</h2>';
 
         for (let i = 0; i < this.products.length; i++) {
             const productRow = this.renderRow(this.products[i]);
             const buyButton = this.renderBuyButton();
+            buyButton.addEventListener('click', this.addProductToBasket);
             productRow.appendChild(buyButton);
             this.catalogContainer.appendChild(productRow);
         }
-
         const totalQuantityOfGoodsRow = this.totalQuantityOfGoodsRow();
         this.catalogContainer.appendChild(totalQuantityOfGoodsRow);
     },
@@ -74,7 +75,65 @@ const catalog = {
 
     getTotalQuantityOfGoods() {
         return this.products.length
-    }
+    },
+
+    addProductToBasket(eventObj) {
+        const eventElement = eventObj.target;
+        return eventElement.parentElement.innerText
+    },
 };
 
-catalog.renderCatalog()
+
+const basket = {
+    catalog,
+    products: [],
+    basketAmount: null,
+    basketContainer: null,
+
+
+    init() {
+        this.basketContainer = document.getElementById('basket');
+        this.basketContainer.innerHTML = '<h2 class="div_title">Basket</h2>';
+
+        for (let i = 0; i < this.products.length; i++) {
+            const productRow = this.renderRow(this.products[i]);
+            this.basketContainer.appendChild(productRow)
+        }
+
+        const totalPriceRow = this.renderTotalPriceRow()
+        this.basketContainer.appendChild(totalPriceRow)
+
+    },
+
+    renderRow(product) {
+        const div = document.createElement('div');
+        div.className = 'product_row'
+        div.innerHTML = `Наименование: ${product.title}; 
+                        Стоимость за ед.: ${product.price}; 
+                        Количество: ${product.quantity};
+                        Полная стоимость товара: ${product.price * product.quantity}`;
+        return div
+    },
+
+    renderTotalPriceRow() {
+        const div = document.createElement('div');
+        div.className = 'total_row'
+        if (this.products.length !== 0) {
+            div.innerHTML = `В корзине товаров: ${this.products.length}, на сумму: ${this.totalBasketPrice()}`;
+        } else {
+            div.innerHTML = 'Корзина пуста';
+        }
+        return div
+    },
+
+    totalBasketPrice() {
+        for (let i = 0; i < this.products.length; i++) {
+            this.basketAmount += (this.products[i].price * this.products[i].quantity)
+        }
+        return +this.basketAmount.toFixed(2)
+    },
+
+};
+
+catalog.init()
+basket.init()
