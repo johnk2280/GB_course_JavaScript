a2e309da5cda111bbd6cac5965ddae7b76db4566
johnk2280 <johnk2280@gmail.com>
2021-07-08 20:37:14 +0500

    [add] added reduceTheNumber

--- lesson_6_hw/task_6_1.js
+++ lesson_6_hw/task_6_1.js
@@ -7,7 +7,7 @@
  */
 
 const catalog = {
-    catalogProducts: [
+    products: [
         {
             'title': 'apple',
             'price': 17.5,
@@ -26,13 +26,12 @@ const catalog = {
     ],
     catalogContainer: null,
 
-
     init() {
         this.catalogContainer = document.getElementById('catalog');
         this.catalogContainer.innerHTML = '<h2 class="div_title">Catalog</h2>';
 
-        for (let i = 0; i < this.catalogProducts.length; i++) {
-            const productRow = this.renderRow(this.catalogProducts[i]);
+        for (let i = 0; i < this.products.length; i++) {
+            const productRow = this.renderRow(this.products[i], i);
             const buyButton = this.renderBuyButton(i);
             buyButton.addEventListener('click', this.addProductToBasket);
             productRow.appendChild(buyButton);
@@ -42,20 +41,20 @@ const catalog = {
         this.catalogContainer.appendChild(totalQuantityOfGoodsRow);
     },
 
-    renderRow(product) {
+    renderRow(product, index) {
         const div = document.createElement('div');
         div.className = 'catalog_product_row';
+        div.id = index;
         div.innerHTML = `Наименование: ${product.title}; 
                         Стоимость за ед.: ${product.price}; 
-                        Количество: ${product.quantity};
-                        Полная стоимость товара: ${(product.price * product.quantity).toFixed(2)}`;
+                        Количество: ${product.quantity}`;
         return div
     },
 
     totalQuantityOfGoodsRow() {
         const div = document.createElement('div');
         div.className = 'catalog_total_quantity_of_goods';
-        if (this.catalogProducts.length !== 0) {
+        if (this.products.length !== 0) {
             div.innerHTML = `Товаров в каталоге: ${this.getTotalQuantityOfGoods()}`;
         } else {
             div.innerHTML = 'В каталоге нет товаров';
@@ -75,20 +74,42 @@ const catalog = {
     },
 
     getTotalQuantityOfGoods() {
-        return this.catalogProducts.length
+        return this.products.length
+    },
+
+    reduceTheNumberOfProducts(index) {
+        catalog.products[index].quantity -= 1;
+    },
+
+    updateRow(parent, productId) {
+        const productRow = this.renderRow(this.products[+productId], +productId);
+        const buyButton = this.renderBuyButton(+productId);
+        buyButton.addEventListener('click', this.addProductToBasket);
+        productRow.appendChild(buyButton);
+        console.log(parent);
+        console.log(productRow)
+        parent.parentElement.replaceChild(productRow, parent);
+
+
+        // parent.textContent = `Наименование: ${catalog.products.title}; Стоимость за ед.: ${catalog.products.price}; Количество: ${catalog.products.quantity}`;
+        // console.log(parent)
+        console.log(catalog.products)
     },
 
     addProductToBasket(eventObj) {
-        const idEventElement = eventObj.target.id;
-        catalog.catalogProducts[idEventElement].quantity -= 1;
-        console.log(catalog.catalogProducts)
+        const eventElement = eventObj.target;
+        catalog.reduceTheNumberOfProducts(eventElement.id);
+        catalog.updateRow(eventElement.parentElement, eventElement.id);
+
+        // console.log(catalog.products)
+        // console.log(eventElement.parentElement)
         // return eventElement.parentElement.innerText
     },
 };
 
 
 const basket = {
-    catalogProducts: [],
+    products: [],
     basketAmount: null,
     basketContainer: null,
 
@@ -97,8 +118,8 @@ const basket = {
         this.basketContainer = document.getElementById('basket');
         this.basketContainer.innerHTML = '<h2 class="div_title">Basket</h2>';
 
-        for (let i = 0; i < this.catalogProducts.length; i++) {
-            const productRow = this.renderRow(this.catalogProducts[i]);
+        for (let i = 0; i < this.products.length; i++) {
+            const productRow = this.renderRow(this.products[i]);
             this.basketContainer.appendChild(productRow)
         }
 
@@ -120,8 +141,8 @@ const basket = {
     renderTotalPriceRow() {
         const div = document.createElement('div');
         div.className = 'total_row'
-        if (this.catalogProducts.length !== 0) {
-            div.innerHTML = `В корзине товаров: ${this.catalogProducts.length}, на сумму: ${this.totalBasketPrice()}`;
+        if (this.products.length !== 0) {
+            div.innerHTML = `В корзине товаров: ${this.products.length}, на сумму: ${this.totalBasketPrice()}`;
         } else {
             div.innerHTML = 'Корзина пуста';
         }
@@ -129,8 +150,8 @@ const basket = {
     },
 
     totalBasketPrice() {
-        for (let i = 0; i < this.catalogProducts.length; i++) {
-            this.basketAmount += (this.catalogProducts[i].price * this.catalogProducts[i].quantity)
+        for (let i = 0; i < this.products.length; i++) {
+            this.basketAmount += (this.products[i].price * this.products[i].quantity)
         }
         return +this.basketAmount.toFixed(2)
     },
