8eff42a958143bce1fc9a96d8fc23af49956a34d
johnk2280 <johnk2280@gmail.com>
2021-07-12 20:09:22 +0500

    [upd] updated lesson_7_hw

--- lesson_7_hw/lesson_7.js
+++ lesson_7_hw/lesson_7.js
@@ -32,7 +32,7 @@ const config = {
 
     validate() {
         const result = {
-            is_valid: true,
+            isValid: true,
             errors: [],
         };
 
@@ -92,9 +92,193 @@ const map = {
             cell.className = 'cell';
         }
 
-    },
+        this.usedCells = [];
 
+        snakePointsArray.forEach((point, index) => {
+            const snakeCell = this.cells[`x${point.x}_y${point.y}`];
+            snakeCell.classList.add(index === 0 ? 'snakeHead' : 'snakeBody');
+            this.usedCells.push(snakeCell);
+        });
+
+        const foodCell = this.cells[`x${foodPoint.x}_y${foodPoint.y}`];
+        foodCell.classList.add('food');
+        this.usedCells.push(foodCell);
+    },
 };
 
+const snake = {
+    body: [],
+    direction: null,
+    lastStepDirection: null,
+
+    init(startBody, direction) {
+        this.body = startBody;
+        this.direction = direction;
+        this.lastStepDirection = direction;
+    },
+
+    getBody () {
+        return this.body;
+    },
+
+    getLastStepDirection() {
+        return this.lastStepDirection;
+    },
+
+    setDirection(direction) {
+        this.direction = direction;
+    },
+
+    isOnPoint(point) {
+        return this.getBody().some((snakePoint) => {
+            return snakePoint.x === point.x && snakePoint.y === point.y;
+        });
+    },
+
+    makeStep() {
+        this.lastStepDirection = this.direction;
+        this.getBody().unshift(this.getNextStepHeadPoint());
+        this.getBody().pop();
+    },
+
+    growUp() {
+        const lastBodyIndex = this.getBody().length - 1;
+        const lastBodyPoint = this.getBody()[lastBodyIndex];
+        const lastBodyPointClone = Object.assign({}, lastBodyPoint);
+
+        this.getBody().push(lastBodyPointClone);
+    },
+
+    getNextStepHeadPoint() {
+        const firstPoint = this.getBody()[0];
+
+        switch (this.direction) {
+            case 'up':
+                return {x: firstPoint.x, y: firstPoint.y - 1};
+            case 'right':
+                return {x: firstPoint.x + 1, y: firstPoint.y};
+            case 'down':
+                return {x: firstPoint.x, y: firstPoint.y + 1};
+            case 'left':
+                return {x: firstPoint.x - 1, y: firstPoint.y};
+        }
+    },
+};
+
+const food = {
+    x: null,
+    y: null,
+
+    getCoordinates() {
+        return {
+            x: this.x,
+            y: this.y
+        };
+    },
+
+    setCoordinates(point) {
+        this.x = point.x;
+        this.y = point.y;
+    },
+
+    isOnPoint(point) {
+        return this.x === point.x && this.y === point.y;
+    },
+};
+
+status = {
+    condition: null,
+
+    setPlaying() {
+        this.condition = 'playing';
+    },
+
+    setStopped() {
+        this.condition = 'stopped';
+    },
+
+    setFinished() {
+        this.condition = 'finished';
+    },
+
+    isPlaying() {
+        return this.condition === 'playing';
+    },
+
+    isStopped() {
+        return this.condition === 'stopped';
+    },
+};
+
+
+const game = {
+    config,
+    map,
+    snake,
+    food,
+    status,
+    tickInterval: null,
+
+    init(userSettings = {}) {
+        this.config.init(userSettings);
+        const validation = this.config.validate();
+
+        if (!validation.isValid) {
+            for (const err of validation.errors) {
+                console.error(err);
+            }
+            return
+        }
+
+        this.map.init(this.config.getRowsCount(), this.config.getColsCount());
+        this.initEventHandlers();
+        this.reset();
+
+    },
+
+    reset() {
+        this.stop();
+        this.snake.init(this.getStartSnakeBody(), 'up');
+        this.food.setCoordinates(this.getRandomFreeCoordinates());
+        this.render();
+    },
+
+    stop() {
+
+    },
+
+    getStartSnakeBody() {
+        return [
+            {
+                x: Math.floor(this.config.getColsCount() / 2),
+                y: Math.floor(this.config.getRowsCount() / 2),
+            }
+        ];
+    },
+
+    getRandomFreeCoordinates() {
+        const exclude = [this.food.getCoordinates(), ...this.snake.getBody()];
+
+        while (true) {
+            const rndPoint = {
+                x: Math.floor(Math.random() * this.config.getColsCount()),
+                y: Math.floor(Math.random() * this.config.getRowsCount()),
+            };
+
+            if (!exclude.some((exPoint) => rndPoint.x === exPoint.x && rndPoint.y === exPoint.y)) return rndPoint;
+        }
+    },
+
+    initEventHandlers() {
+
+    },
+
+    render() {
+        this.map.render(this.snake.getBody(), this.food.getCoordinates())
+    },
+};
+
+
+game.init({speed:5})
 
 
