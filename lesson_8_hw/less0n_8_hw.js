fd68623514609796715533b1d1fa92e832dc369f
johnk2280 <johnk2280@gmail.com>
2021-07-13 18:09:20 +0500

    [add] added task_8_2

--- lesson_8_hw/less0n_8_hw.js
+++ lesson_8_hw/less0n_8_hw.js
@@ -1,1 +1,33 @@
 'use strict'
+
+
+/**
+ * 2. Не выполняя кода, ответить, что выведет браузер и почему:
+
+ if (!("a" in window)) {
+    var a = 1;
+}
+ alert(a);
+
+ Т.к. переменная 'a' ранее не была объявлена, то alert выведет undefined
+
+ _____________________________________________________
+
+ var b = function a(x) {
+    x && a(--x);
+};
+ alert(a);
+ function a(x) {
+    return x * 2;
+}
+ var a;
+ alert(a);
+ function b(x, y, a) {
+    arguments[2] = 10;
+    alert(a);
+}
+ b(1, 2, 3);
+ function a() {
+    alert(this);
+}
+ */
